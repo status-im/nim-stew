8fe16bcd9d0491c36681268dc5468a3f191fbcfa
Zahary Karadjov <zahary@gmail.com>
2019-07-03 02:30:49 +0300

    Add default(T)

--- std_shims/objects.nim
+++ std_shims/objects.nim
@@ -14,3 +14,6 @@ template init*(lvalue: var auto, a1, a2, a3: auto) =
   mixin init
   lvalue = init(type(lvalue), a1, a2, a3)
 
+when not declared(default):
+  proc default*(T: type): T = discard
+
