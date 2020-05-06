05b3b486842d3f4cf8e9d910818aaa2ca6eb7a30
Jacek Sieka <jacek@status.im>
2020-05-06 12:09:41 +0200

    byteutils: compile-time to/fromBytes

--- stew/byteutils.nim
+++ stew/byteutils.nim
@@ -145,12 +145,22 @@ func toBytes*(s: string): seq[byte] =
   ## Convert a string to the corresponding byte sequence - since strings in
   ## nim essentially are byte sequences without any particular encoding, this
   ## simply copies the bytes without a null terminator
-  @(s.toOpenArrayByte(0, s.high))
+  when nimvm:
+    var r = newSeq[byte](s.len)
+    for i, c in s:
+      r[i] = cast[byte](c)
+    r
+  else:
+    @(s.toOpenArrayByte(0, s.high))
 
 func fromBytes*(T: type string, v: openArray[byte]): string =
   if v.len > 0:
     result = newString(v.len)
-    copyMem(addr result[0], unsafeAddr v[0], v.len)
+    when nimvm:
+      for i, c in v:
+        result[i] = cast[char](c)
+    else:
+      copyMem(addr result[0], unsafeAddr v[0], v.len)
 
 func `<`*(a, b: openArray[byte]): bool =
   ## Lexicographical compare of two byte arrays
