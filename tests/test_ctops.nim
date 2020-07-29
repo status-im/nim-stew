6c0380b3e6b70722f76e78eb26132c40619eb0a1
cheatfate <eugene.kabanov@status.im>
2020-07-29 21:29:46 +0300

    Allow any integer type to use CT.isEqual(). Add tests for different integer types.

--- tests/test_ctops.nim
+++ tests/test_ctops.nim
@@ -28,6 +28,25 @@ suite "Constant-time operations test suite":
       seq4 = @byteArr4
       seq5 = @byteArr5
 
+      int8Arr = [1'i8, 2'i8, 3'i8]
+      int16Arr = [1'i16, 2'i16, 3'i16]
+      int32Arr = [1'i32, 2'i32, 3'i32]
+      intArr = [1, 2, 3]
+
+      uint8Arr = [1'u8, 2'u8, 3'u8]
+      uint16Arr = [1'u16, 2'u16, 3'u16]
+      uint32Arr = [1'u32, 2'u32, 3'u32]
+      uintArr = [1'u, 2'u, 3'u]
+
+      emptyArray: array[0, byte] = []
+      emptyString = ""
+      emptySeq = newSeq[byte]()
+
+    when sizeof(int) == 8:
+      let
+        int64Arr = [1'i64, 2'i64, 3'i64]
+        uint64Arr = [1'u64, 2'u64, 3'u64]
+
     check:
       CT.isEqual(charArr1, charArr1) == true
       CT.isEqual(charArr1, byteArr1) == true
@@ -53,3 +72,33 @@ suite "Constant-time operations test suite":
       CT.isEqual(str1, byteArr5) == false
       CT.isEqual(seq1, str5) == false
       CT.isEqual(charArr1, seq5) == false
+
+      CT.isEqual(int8Arr, int8Arr) == true
+      CT.isEqual(int8Arr, uint8Arr) == true
+      CT.isEqual(int16Arr, int16Arr) == true
+      CT.isEqual(int16Arr, uint16Arr) == true
+      CT.isEqual(int32Arr, int32Arr) == true
+      CT.isEqual(int32Arr, uint32Arr) == true
+      CT.isEqual(intArr, intArr) == true
+      CT.isEqual(intArr, uintArr) == true
+
+    when sizeof(int) == 8:
+      check:
+        CT.isEqual(int64Arr, int64Arr) == true
+        CT.isEqual(int64Arr, uint64Arr) == true
+
+    # Empty arrays
+    expect(AssertionError):
+      discard CT.isEqual(emptyArray, emptyArray)
+    expect(AssertionError):
+      discard CT.isEqual(emptyArray, emptyString)
+    expect(AssertionError):
+      discard CT.isEqual(emptyArray, emptySeq)
+
+    # Arrays, where T is different type size
+    expect(AssertionError):
+      discard CT.isEqual(int8Arr, int16Arr)
+    expect(AssertionError):
+      discard CT.isEqual(int16Arr, int32Arr)
+    expect(AssertionError):
+      discard CT.isEqual(int8Arr, intArr)
