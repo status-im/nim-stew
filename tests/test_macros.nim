04f8150a080e2db4ddd7959ba89549f45494af80
Gabben <43146729+gabbhack@users.noreply.github.com>
2021-02-10 18:10:08 +0500

    Fix #62 (#75)
    * Fix #62
    
    - Supports more than one pragma parameter (return tuple)
    - Returns nil if there are zero parameters in the pragma (like std)

--- tests/test_macros.nim
+++ tests/test_macros.nim
@@ -1,7 +1,23 @@
 import
+  unittest,
   ../stew/shims/macros
 
+
+template unknown() {.pragma.}
+template zero() {.pragma.}
+template one(one: string) {.pragma.}
+template two(one: string, two: string) {.pragma.}
+
 type
+  MyType[T] = object
+    myField {.zero, one("foo"), two("foo", "bar")}: string
+    myGeneric {.zero.}: T
+    case kind {.zero.}: bool
+      of true:
+        first {.zero.}: string
+      else:
+        second {.zero.}: string
+
   FieldKind = enum
     KindA
     KindB
@@ -40,3 +56,29 @@ static:
     "anotherDerivedField"
   ]
 
+let myType = MyType[string](myField: "test", myGeneric: "test", kind: true, first: "test")
+
+suite "Macros":
+  test "hasCustomPragmaFixed":
+    check:
+      not myType.type.hasCustomPragmaFixed("myField", unknown)
+      myType.type.hasCustomPragmaFixed("myField", zero)
+      myType.type.hasCustomPragmaFixed("myField", one)
+      myType.type.hasCustomPragmaFixed("myField", two)
+
+      myType.type.hasCustomPragmaFixed("myGeneric", zero)
+      myType.type.hasCustomPragmaFixed("kind", zero)
+      myType.type.hasCustomPragmaFixed("first", zero)
+      myType.type.hasCustomPragmaFixed("second", zero)
+
+  test "getCustomPragmaFixed":
+    check:
+      myType.type.getCustomPragmaFixed("myField", unknown).isNil
+      myType.type.getCustomPragmaFixed("myField", zero).isNil
+      myType.type.getCustomPragmaFixed("myField", one) is string
+      myType.type.getCustomPragmaFixed("myField", two) is tuple[one: string, two: string]
+
+      myType.type.getCustomPragmaFixed("myGeneric", zero).isNil
+      myType.type.getCustomPragmaFixed("kind", zero).isNil
+      myType.type.getCustomPragmaFixed("first", zero).isNil
+      myType.type.getCustomPragmaFixed("second", zero).isNil
