2374b2d27a4afa36dbd0b03aa62faefd1f327a58
Jacek Sieka <jacek@status.im>
2023-05-11 13:35:05 +0200

    comment

--- tests/test_results.nim
+++ tests/test_results.nim
@@ -377,6 +377,7 @@ block: # Result[T, void] aka `Opt`
   doAssert Opt.some(42).get() == 42
   doAssert Opt.none(int).isNone()
 
+  # Construct Opt from Result
   doAssert oOk.orErr("error").value() == oOk.get()
   doAssert oErr.orErr("error").error() == "error"
 
