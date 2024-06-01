8338b2fb04c9bc1c01266643ef30488300f81cc4
tersec <tersec@users.noreply.github.com>
2024-06-01 22:20:03 +0000

    deprecate stew/results

--- stew/results.nim
+++ stew/results.nim
@@ -1,9 +1,7 @@
 # This module has graduated from stew and is now available from the
 # `results` nimble package instead (https://github.com/arnetheduck/nim-results)
 
-when defined(stewWarnResults):
-  # This deprecation notice will be made default in some future stew commit
-  {.deprecated: "`stew/results` is now availabe as `import results` via the `results` Nimble package".}
+{.deprecated: "`stew/results` is now availabe as `import results` via the `results` Nimble package".}
 
 import pkg/results
 export results
