ff516dfe77f02805bc4e8197d3154a2b11800501
tersec <tersec@users.noreply.github.com>
2024-06-01 22:26:03 +0000

    Update stew/results.nim

--- stew/results.nim
+++ stew/results.nim
@@ -1,7 +1,7 @@
 # This module has graduated from stew and is now available from the
 # `results` nimble package instead (https://github.com/arnetheduck/nim-results)
 
-{.deprecated: "`stew/results` is now availabe as `import results` via the `results` Nimble package".}
+{.deprecated: "`stew/results` is now available as `import results` via the `results` Nimble package".}
 
 import pkg/results
 export results
