c158d32d07391889adcc05e3c34a6e64fbbe574f
Constantine Molchanov <moigagoo@duck.com>
2026-02-12 19:15:34 +0400

    Depracate saturating_arith and saturation_arith.

--- stew/saturation_arith.nim
+++ stew/saturation_arith.nim
@@ -1,7 +1,19 @@
 ## This module may hold various saturating arithmetic definitions.
 ## It will be expanded on demand as Status project require its definition
 
+{.deprecated.}
+
 func saturate*(T: type int64, u: uint64): T =
+  ##[ Convert a uint64 to int64. If the value won't fit in int64,
+  return maximal value.
+
+  Notice: This function is deprecated.
+
+  If you need to limit the value of an integer, use `clamp(a, minVal, maxVal)` from stdlib.
+
+  If you use this function to implement saturating addition, use `saturatingAdd` from [intops](https://github.com/vacp2p/nim-intops/) library.
+  ]##
+
   if u > high(int64).uint64:
     high(int64)
   else:
