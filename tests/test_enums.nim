c6a283472995dde660f09146bae1c63a6e1ff9b4
nitely <ecastroborsani@gmail.com>
2026-04-29 16:19:05 -0300

    undo

--- tests/test_enums.nim
+++ tests/test_enums.nim
@@ -160,15 +160,10 @@ suite "enums":
         B4 = 2000
         C4 = 4000
 
-      WithLimits32 = enum
+      WithLimits = enum
         A5 = int32.low()
         B5 = 0
         C5 = int32.high()
-
-      WithLimits64 = enum
-        A6 = int64.low()
-        B6 = 0
-        C6 = int64.high()
 
     check:
       hasHoles(EnumWithOneValue) == false
@@ -176,8 +171,7 @@ suite "enums":
       hasHoles(WithoutHoles2) == false
       hasHoles(WithHoles) == true
       hasHoles(WithBigHoles) == true
-      hasHoles(WithLimits32) == true
-      hasHoles(WithLimits64) == true
+      hasHoles(WithLimits) == true
 
   test "checkedEnumAssign":
     type
