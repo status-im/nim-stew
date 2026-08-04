153a16bdd5216d73c219944357bd620dd43cffbb
tersec <tersec@users.noreply.github.com>
2026-08-04 12:00:54 +0000

    add string setLenUninit shim handling

---
+++ stew/shims/struninit.nim
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+# Copyright (c) Status Research & Development GmbH
+
+{.push raises: [], gcsafe.}
+{.used.}
+
+# https://github.com/nim-lang/Nim/pull/24836
+# https://github.com/nim-lang/Nim/pull/25022
+# https://github.com/nim-lang/Nim/pull/25767
+# TODO if Nim 2.2.12 gets a 25767 backport, allow in refc here
+when (NimMajor, NimMinor, NimPatch) < (2, 2, 10) or
+     ((NimMajor, NimMinor) < (2, 4) and defined(gcRefc)):
+  template setLenUninit*(s: var string, newlen: Natural) =
+    setLen(s, newLen)
