59a605585d7e75b2851e68716443044104c95b6e
tersec <tersec@users.noreply.github.com>
2026-08-04 13:07:48 +0000

    introduce new symbol setLenUninit2

--- stew/shims/struninit.nim
+++ stew/shims/struninit.nim
@@ -7,8 +7,12 @@
 # https://github.com/nim-lang/Nim/pull/24836
 # https://github.com/nim-lang/Nim/pull/25022
 # https://github.com/nim-lang/Nim/pull/25767
-# TODO if Nim 2.2.12 gets a 25767 backport, allow in refc here
-when (NimMajor, NimMinor, NimPatch) < (2, 2, 10) or
-     ((NimMajor, NimMinor) < (2, 4) and defined(gcRefc)):
-  template setLenUninit*(s: var string, newlen: Natural) =
+# can't properly override broken-but-extant setLenUninit with any of
+# proc/func/template, so use setLenUninit2
+# TODO if Nim 2.2.12 gets a 25767 backport, allow in refc
+template setLenUninit2*(s: var string, newlen: Natural) =
+  when (NimMajor, NimMinor, NimPatch) < (2, 2, 10) or
+       ((NimMajor, NimMinor) < (2, 4) and defined(gcRefc)):
     setLen(s, newLen)
+  else:
+    setLenUninit(s, newLen)
